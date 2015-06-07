f9d51cd408caa4fb3fc190d10af5b7a6591826d9
Piyush Waradpande <pi5@umbc.edu>
2015-06-06 21:03:03 -0400

    async io

--- my_first_async_io.js
+++ my_first_async_io.js
@@ -2,7 +2,7 @@
 
 var fs = require('fs');
 
-var buf = fs.readFile(process.argv[2], function(err, data) {
+fs.readFile(process.argv[2], function(err, data) {
     if(err) throw err;
     var lines = data.toString().split('\n').length - 1;
     console.log(lines);
